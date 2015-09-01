114a258477cf2d8fa58788245d03fdc00bb246f0
Dmitry Buslaev <info@buslaev.com>
2015-08-31 22:03:35 -0700

    pulls first questions

--- run.rb
+++ run.rb
@@ -20,29 +20,40 @@ good_questions=[] # holds all questions on the output
 used_strands=[]
 used_standards=[]
 used_questions=[]
+
 i=0
 while i < total_questions # loop collecting questions
 	usable_questions=[]
+	usable_strands_questions=[]
 	csv_array.each do |row| # I am going to use numeric parameters, time preasure...
 		if not used_strands.include? row[0]
+			usable_strands_questions.push(row)
+		end
+	end
+	if usable_strands_questions.length == 0 # go back to full list
+		used_strands=[]
+		usable_strands_questions=csv_array
+	end
+	usable_strands_questions.each do |row|
+		if not used_standards.include? row[2] # check if standard was used
 			usable_questions.push(row)
 		end
 	end
-	if usable_questions.length>0 # if something was found that has not been used
+	if usable_questions.length == 0 # reset and run again, standard ids depleted
+		used_strands=[]
+		used_standards=[]
+	else
 		to_use=usable_questions[0]
+		
 		good_questions.push(to_use)
 		used_strands.push(to_use[0]) # strand id
 		used_standards.push(to_use[2]) # standard id
 		used_questions.push(to_use[4]) # question id
 		i+=1
-	else # no matches, get anything
-		used_strands=[]
-		used_standards=[]
-		used_questions=[]
 	end
-	usable_questions.each do |row|
-		puts row[0]
-	end
+end
+good_questions.each do |row|
+	puts row[4]
 end
 
 
