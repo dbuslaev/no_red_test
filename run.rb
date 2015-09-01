676df3802126eecc3c1a0e82cf31043af31ec5bf
Dmitry Buslaev <info@buslaev.com>
2015-08-31 21:09:47 -0700

    starting with questions

--- run.rb
+++ run.rb
@@ -2,13 +2,20 @@ require 'rubygems'
 require 'json'
 require 'csv'
 
+total_questions=3;
+
 csv_text = File.read('questions.csv')
-csv_array = JSON.parse(CSV.parse(csv_text).to_json)
+csv_array = JSON.parse(CSV.parse(csv_text).to_json) # reading into array
 
 csv_array.each do |row|
 	row.each do |column|
-		puts column
+#		puts column
 	end
 end
+
+(0..(total_questions-1)).each do |i| # loop collecting questions
+	puts "# #{i}"
+end
+
 
 puts "Done"
