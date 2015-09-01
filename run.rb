b5514f6442152a683ea152f35543db5ed317493e
Dmitry Buslaev <info@buslaev.com>
2015-08-31 21:47:31 -0700

    infinite loop

--- run.rb
+++ run.rb
@@ -11,14 +11,33 @@ csv_ids=csv_array[0]
 csv_array.shift()
 csv_array.each do |row|
 	row.each do |column|
-		puts column
+#		puts column
 	end
 end
 
 used_info={} # using hashes to track numeric counters for each strand
 
-(0..(total_questions-1)).each do |i| # loop collecting questions
-	puts "# #{i}"
+used_strands=[]
+used_standards=[]
+used_questions=[]
+i=0
+while i < total_questions # loop collecting questions
+	usable_questions=[]
+	csv_array.each do |row| # I am going to use numeric parameters, time preasure...
+		if not used_strands.include? row[0]
+			usable_questions.push(row)
+		end
+	end
+	if usable_questions.length>0 # if something was found that has not been used
+		i+=1
+	else # no matches, get anything
+		used_strands=[]
+		used_standards=[]
+		used_questions=[]
+	end
+#	usable_questions.each do |row|
+#		puts row[0]
+#	end
 end
 
 
