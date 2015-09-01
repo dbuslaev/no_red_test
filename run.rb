2538d1f4d587dad412cc4efb98a09be2c5fc9ef3
Dmitry Buslaev <info@buslaev.com>
2015-08-31 21:30:09 -0700

    work in progress

--- run.rb
+++ run.rb
@@ -7,11 +7,15 @@ total_questions=3;
 csv_text = File.read('questions.csv')
 csv_array = JSON.parse(CSV.parse(csv_text).to_json) # reading into array
 
+csv_ids=csv_array[0]
+csv_array.shift()
 csv_array.each do |row|
 	row.each do |column|
-#		puts column
+		puts column
 	end
 end
+
+used_info={} # using hashes to track numeric counters for each strand
 
 (0..(total_questions-1)).each do |i| # loop collecting questions
 	puts "# #{i}"
