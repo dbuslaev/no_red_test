c06925faf32784cc28842ca94652ad9d0d57d184
Dmitry Buslaev <info@buslaev.com>
2015-08-31 21:04:18 -0700

    reading questions

--- run.rb
+++ run.rb
@@ -1,1 +1,14 @@
-puts "Hello"
+require 'rubygems'
+require 'json'
+require 'csv'
+
+csv_text = File.read('questions.csv')
+csv_array = JSON.parse(CSV.parse(csv_text).to_json)
+
+csv_array.each do |row|
+	row.each do |column|
+		puts column
+	end
+end
+
+puts "Done"
