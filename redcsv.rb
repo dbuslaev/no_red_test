e957687422753c158905d86bba8c2131c47db73e
Dmitry Buslaev <info@buslaev.com>
2015-09-03 23:13:02 -0700

    cleaning up

--- redcsv.rb
+++ redcsv.rb
@@ -1,18 +1,18 @@
 module Redcsv
 
-	@data={}
+  @data = {}
 
-	def read(filename) # read dataset from the file
-		csv_text = File.read(filename)
+  def read(filename) # read dataset from the file
+    csv_text = File.read(filename)
 
-		CSV::Converters[:blank_to_nil] = lambda do |field|
-			field && field.empty? ? nil : field
-		end
-		csv = CSV.new(csv_text, :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
-		@data=csv.to_a.map{|row| row.to_hash}
-	end
+    CSV::Converters[:blank_to_nil] = lambda do |field|
+      field && field.empty? ? nil : field
+    end
+    csv = CSV.new(csv_text, :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
+    @data = csv.to_a.map{ |row| row.to_hash }
+  end
 
-	def get() # return saved dataset
-		@data;
-	end
+  def get() # return saved dataset
+    @data
+  end
 end
--- redquestion.rb
+++ redquestion.rb
@@ -7,60 +7,64 @@ include Redcsv
 		read(filename) # read csv file
 	end
 	
-	def generate(total_questions)
-		csv_array=get()
-		good_questions=[] # holds all questions on the output
-		used_strands=[]
-		used_standards=[]
-		i=0
-		while i < total_questions # loop collecting questions
-			usable_questions=[]
-			usable_strands_questions=[]
-			csv_array.each do |row| # I am going to use numeric parameters, time preasure...
-				if not used_strands.include? row[:strand_id]
-		#	puts row[:strand_id]
+	def generate(total_questions) # generate set of questions
+		all_questions = get()
+		good_questions = [] # holds all questions on the output
+		used_strands = [] # tracks repeat usage
+		used_standards = [] # tracks repeat usage
+		i = 0
+		while true # loop collecting questions
+			usable_questions = []
+			usable_strands_questions = []
+			all_questions.each do |row|
+				unless used_strands.include? row[:strand_id]
 					usable_strands_questions.push(row)
 				end
 			end
 			if usable_strands_questions.length == 0 # go back to full list
-				used_strands=[]
-				usable_strands_questions=csv_array
+				used_strands = []
+				usable_strands_questions = all_questions
 			end
 			usable_strands_questions.each do |row|
-				if not used_standards.include? row[:standard_id] # check if standard was used
+				unless used_standards.include? row[:standard_id] # check if standard was used
 					usable_questions.push(row)
 				end
 			end
 			if usable_questions.length == 0 # reset and run again, standard ids depleted
-				used_strands=[]
-				used_standards=[]
+				used_strands = []
+				used_standards = []
 			else
-				latest_usable_questions=[]
+				unique_questions = []
 				usable_questions.each do |row|
-					question_id=row[:question_id].to_s
-#					if not usage_question_hash[question_id].nil?
-#						if usage_question_hash[question_id][:assigned]!=true and usage_question_hash[question_id][:answered]!=true
-#							latest_usable_questions.push(row)
-#						end
+					question_id = row[:question_id].to_s
+#					if usage_question_hash[question_id].nil?
+						unique_questions.push(row)
 #					else
-						latest_usable_questions.push(row)
+#						if usage_question_hash[question_id][:assigned] != true and usage_question_hash[question_id][:answered] != true
+#							unique_questions.push(row)
+#						end
 #					end
 				end
-				if latest_usable_questions.length == 0
-					latest_usable_questions=usable_questions
+				if unique_questions.length == 0
+					unique_questions = usable_questions
 				end
-				id_to_use=Random.rand(0..(latest_usable_questions.length-1))
-				to_use=latest_usable_questions[id_to_use]
+				id_to_use = Random.rand(0..(unique_questions.length-1))
+				use_question = unique_questions[id_to_use]
 				
-				good_questions.push(to_use)
-				used_strands.push(to_use[:strand_id]) # strand id
-				used_standards.push(to_use[:standard_id]) # standard id
-				question_id=to_use[:question_id].to_s
+				good_questions.push(use_question)
+				used_strands.push(use_question[:strand_id]) # strand id
+				used_standards.push(use_question[:standard_id]) # standard id
+				question_id = use_question[:question_id].to_s
 #				if usage_question_hash[question_id].nil?
-#					usage_question_hash[question_id]={:answered=>false}
+#					usage_question_hash[question_id] = {
+#						:answered => false
+#					}
 #				end
-#				usage_question_hash[question_id][:assigned]=true;
-				i+=1
+#				usage_question_hash[question_id][:assigned] = true
+				i += 1
+				if i >= total_questions
+					break
+				end
 			end
 		end
 		good_questions
